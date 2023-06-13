6c873a356285d442bc00579eacd1f898ff991d0d
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-06-13 10:27:03 +0300

    project

---
+++ location.js
@@ -0,0 +1,94 @@
+
+let locations = document.getElementById('my-location')
+function getLocation (){
+    fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=6f62c111289b4a94a74bc75122ae2d84')
+    .then(response => response.json())
+    .then(response => {
+        console.log({response});
+       locations.innerHTML+= JSON.stringify(response)
+    })
+    .catch(error=> new Error(error.message))
+}
+getLocation();
+
+
+
+navigator.geolocation.getCurrentPosition(function(position) {
+
+    
+    if (position.accuracy < 100) {
+     
+      var notification = new Notification("Subscribe to get seasonal suprises every week");
+      notification.onclick = function() {
+        
+        window.location.href = "https://spectacular-blini-ddc0d9.netlify.app/";
+      };
+    } else {
+     
+      alert("Expanding our coverage to reach you better");
+    }
+  
+  });
+
+
+
+
+
+  const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14;
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
+    });
+});
+const autoSlide = () => {
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff);
+    let firstImgWidth = firstImg.clientWidth + 14;
+    let valDifference = firstImgWidth - positionDiff;
+    if(carousel.scrollLeft > prevScrollLeft) {
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+
+
+
+
